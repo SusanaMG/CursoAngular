03c8a2ba4e5ecf1857f8499d5caa616aa2336aeb
susanamg <susana.marquez@atos.net>
2019-08-12 14:53:50 +0200

    02 Data Binding

--- appCurso/src/app/app.module.ts
+++ appCurso/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FechaactualComponent } from './fechaactual/fechaactual.component';
 import { CopyrightComponent } from './copyright/copyright.component';
+import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
+import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
+import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybinding.component';
+import { EjeventbindingComponent } from './ejeventbinding/ejeventbinding.component';
+import { Ej2waybingingComponent } from './ej2waybinging/ej2waybinging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FechaactualComponent,
-    CopyrightComponent
+    CopyrightComponent,
+    ViewmodeloComponent,
+    EjmetodoComponent,
+    EjpropertybindingComponent,
+    EjeventbindingComponent,
+    Ej2waybingingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ appCurso/src/app/ej2waybinging/ej2waybinging.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ej2waybinging',
+  templateUrl: './ej2waybinging.component.html',
+  styleUrls: ['./ej2waybinging.component.css']
+})
+export class Ej2waybingingComponent implements OnInit {
+
+  texto = 'Texto original al cargar...';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
