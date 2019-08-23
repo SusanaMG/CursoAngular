ef7e3b87d255d03af2842d94c02f57a626e683f7
susanamg <susana.marquez@atos.net>
2019-08-23 15:11:45 +0200

    07 Formularios PARTE 1

--- appCompras/src/app/app.module.ts
+++ appCompras/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import { AddpresComponent } from './presupuestos/addpres/addpres.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'addprovee', component: AddproveeComponent },
+  { path: 'addpres', component: AddpresComponent },
   { path: '**', component: InicioComponent }
-];
+  ];
 
 @NgModule({
   declarations: [
@@ -20,10 +26,14 @@ const routes: Routes = [
     ProveedoresComponent,
     InicioComponent,
     HeaderComponent,
+    AddproveeComponent,
+    AddpresComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
